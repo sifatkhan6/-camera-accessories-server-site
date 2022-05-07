273663c55c849756269e1f2ebbbce5ce92e9d84e
MD SIFAT KHAN <sifatkhann229@gmail.com>
2022-05-08 07:17:23 +0800

    delete api implemented

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ async function run() {
             const newItem = req.body;
             const result = await inventoryCollection.insertOne(newItem);
             res.send(result);
+        });
+
+        app.delete('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await inventoryCollection.deleteOne(query);
+            res.send(result);
         })
     }
     finally {
